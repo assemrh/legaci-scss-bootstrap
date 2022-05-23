45e09bc59b21497cabbdc2e6ee9a83136829dd7c
Asim Rahal <assem.rh@gmail.com>
2022-05-23 08:25:16 +0300

    up

--- components/header.js
+++ components/header.js
@@ -18,7 +18,7 @@ function Header() {
                 // width={500} automatically provided
                 // height={500} automatically provided
                 // blurDataURL="data:..." automatically provided
-                // placeholder="blur" // Optional blur-up while loading
+                placeholder="blur" // Optional blur-up while loading
               />
             </div>
             <div className={styles.btn_signup_container}>
--- components/sectionOne.js
+++ components/sectionOne.js
@@ -4,11 +4,11 @@ import { Card, Col, Container, Row } from "react-bootstrap";
 
 function SectionOne() {
   return (
-    <section className={styles.section_1}>
+    <section className={styles.section}>
       <Container>
         <Row>
           <Col>
-            <Card className="border-0 shadow-0 bg-transparent text-center">
+            <Card className={`${styles.card} border-0 shadow-0 bg-transparent text-center`}>
               <Card.Body>
                 <h1>The New Era Of Memory Sharing!</h1>
                 <h3>Build And Share Your Memories Like Never Before!</h3>
